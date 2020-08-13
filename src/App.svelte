bfad6bf65545b37f72c7d8536c7b47b37513e636
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-08-13 22:02:08 +0800

    Modify app

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,5 @@
 <script>
-import CharityList from './CharityList.svelte'
-
-	let name = "Mari Berbagi"
-	setTimeout(function() {
-		name = "Mari Berdonasi"
-	}, 2000);
-
-	let charities = ['Charity 1', 'Charity 2', 'Charity 3'];
+import Home from './pages/Home.svelte'
 </script>
 
-<main>
-	<h1>{name}</h1>
-	<CharityList {charities}/>
-</main>
-
-<style>
-	main {
-		/*text-align: center;*/
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-		border: 0.5em solid #1a1a1a;
-	}
-
-	h1 {
-		color: #ff3e00;
-		font-size: 4em;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Home />
