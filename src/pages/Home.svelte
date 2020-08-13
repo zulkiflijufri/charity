a5c0ea2bc43d401663ef58a416a8551a0c564c12
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-08-13 22:01:47 +0800

    Component statefull

---
+++ src/pages/Home.svelte
@@ -0,0 +1,36 @@
+<script>
+import CharityList from '../components/CharityList.svelte'
+
+	let name = "Mari Berbagi"
+	setTimeout(function() {
+		name = "Mari Berdonasi"
+	}, 2000);
+
+	let charities = ['Charity 1', 'Charity 2', 'Charity 3'];
+</script>
+
+<main>
+	<h1>{name}</h1>
+	<CharityList {charities}/>
+</main>
+
+<style>
+	main {
+		/*text-align: center;*/
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+		border: 0.5em solid #1a1a1a;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-size: 4em;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
