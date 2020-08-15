58063bf9da4ba708b354d309714dd8fe70813443
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-08-15 09:15:25 +0800

    add routing library page.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,16 @@
 <script>
+import router from 'page'
 import Home from './pages/Home.svelte'
 import About from './pages/About.svelte'
 import Contact from './pages/Contact.svelte'
+
+let page;
+
+router('/', () => (page = Home))
+router('/about', () => (page = About))
+router('/contact', () => (page = Contact))
+
+router.start()
 </script>
 
-<Contact />
+<svelte:component this={page}></svelte:component>
