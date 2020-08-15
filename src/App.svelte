d6ea407e0645c634a93e54ab0732c3e7efd0b5aa
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-08-15 11:03:48 +0800

    add route donation with params

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,19 @@ import router from 'page'
 import Home from './pages/Home.svelte'
 import About from './pages/About.svelte'
 import Contact from './pages/Contact.svelte'
+import Donation from './pages/Donation.svelte'
 
-let page;
+let page, params;
 
 router('/', () => (page = Home))
 router('/about', () => (page = About))
 router('/contact', () => (page = Contact))
+router('/donation/:id', function(ctx, next) {
+	params = ctx.params;
+	next();
+}, () => (page = Donation))
 
 router.start()
 </script>
 
-<svelte:component this={page}></svelte:component>
+<svelte:component this={page} params={params}></svelte:component>
